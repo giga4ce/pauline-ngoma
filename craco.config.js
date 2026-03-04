95bb9e4726b176577c86963bbd948f2627755a4e
Harou <harouguindja@gmail.com>
2026-03-04 23:34:01 +0100

    chore: add @ alias via craco + jsconfig

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,12 @@
+const path = require('path')
+
 module.exports = {
   reactScriptsVersion: "react-scripts",
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   style: {
     css: {
       loaderOptions: () => {
