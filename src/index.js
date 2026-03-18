1e05c58cd44247e2ec83cc105779457a5e8bfef9
Harou <harouguindja@gmail.com>
2026-03-19 00:11:12 +0100

    fix : use relative import for global stylesheet

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import '@/styles/main.scss'
+import './styles/main.scss'
 import AppRouter from '@/app/AppRouter'
 
 export const App = () => {
